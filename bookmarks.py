134d8f938386113b843efa0d4a75b7c87d05f61b
Jan Müller <jpmueller@gmx.net>
2013-03-14 16:11:43 +0100

    Removed sorting by visit count because the uid controls ordering (thanks to David Ferguson for pointing that out).

--- bookmarks.py
+++ bookmarks.py
@@ -15,19 +15,16 @@ def places(profile):
 def result(db, query):
     return (
         (uid, url, title, url, u'icon.png') for (uid, title, url) in
-        (item[:3] for item in db.execute(sql(query)))
+        db.execute(sql(query))
     )
 
 def sql(query):
-    queryTemplate = u"""\
-%s
-order by visit_count desc"""
     subqueryTemplate = u"""\
-select moz_places.id, moz_places.title, moz_places.url, moz_places.visit_count from moz_places
+select moz_places.id, moz_places.title, moz_places.url from moz_places
 %s
 where %s"""
     joinTemplate = u'inner join %(table)s on moz_places.id = %(table)s.%(field)s'
-    return queryTemplate % u'\nunion\n'.join(
+    return u'\nunion\n'.join(
         subqueryTemplate % (joinTemplate % locals(), where(query))
         for (table, field) in [(u'moz_inputhistory', u'place_id'), (u'moz_bookmarks', u'id')]
     )
